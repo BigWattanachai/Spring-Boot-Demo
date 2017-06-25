a58b2ed93d80b03c21aa90ead164ce7813dc8ea9
BiG.KMITL <templeboy1992@gmail.com>
2017-06-25 12:53:13 +0700

    Added default constructor to exception class

--- src/main/java/com/ascend/springbootdemo/exceptions/AuthorNotFoundException.java
+++ src/main/java/com/ascend/springbootdemo/exceptions/AuthorNotFoundException.java
@@ -3,8 +3,13 @@ package com.ascend.springbootdemo.exceptions;
 /**
  * Created by BiG on 6/17/2017 AD.
  */
+
 public class AuthorNotFoundException extends RuntimeException {
     public AuthorNotFoundException(String message) {
         super(message);
+    }
+
+    public AuthorNotFoundException() {
+        this("Author Not Found");
     }
 }
--- src/main/java/com/ascend/springbootdemo/exceptions/PostNotFoundException.java
+++ src/main/java/com/ascend/springbootdemo/exceptions/PostNotFoundException.java
@@ -7,4 +7,8 @@ public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(String message) {
         super(message);
     }
+
+    public PostNotFoundException() {
+        this("Post Not Found");
+    }
 }
